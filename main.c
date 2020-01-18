daa605e42ce8f38dab0ad1a422aa181875bcac5d
LetsFailYourGame <teerteufel12345@gmail.com>
2020-01-18 15:22:25 +0000

    Added Comments

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-//Files
+//Filestrean for userinput
 char file_name;                     //file_name var. init.
 char *file_ptr = &file_name;        //pointer to file_name
 
-//Analyse
+//Analyse vars.
 int letters[26];
 int summ = 0;
 int val;
@@ -12,7 +12,7 @@ int val;
 void input_file_name()
 {
   printf("Input Filename to read: ");       // print statement
-  scanf("%s", &file_name);          // put user input into file_name
+  scanf("%s", &file_name);                  // put user input into file_name
 }
 
 void analyseText()
@@ -21,51 +21,62 @@ void analyseText()
   char ch;
   int converted;
 
-  FILE *f = fopen(file_ptr, "r"); // Init. file stream var.
+  FILE *f = fopen(file_ptr, "r");           // Init. file stream var.
 
   // START ****************************************************************
+  
+  // ERROR handling
   if (f == NULL){ printf("\nERROR 404 : [File is not available]\n"); return; }
   else
   {
       printf("\n~ Reading: [ %s ]\n", file_ptr);
 
-      while ((ch = fgetc(f)) != EOF) // While not End Of File read every char by char
+      while ((ch = fgetc(f)) != EOF)        // While not End Of File read every char by char
       {
-        converted = (int)ch; // Convert char by char into (int) ASCII val. 
+        converted = (int)ch;                // Convert char by char into (int) ASCII val. 
 
         while(fscanf(f, "%d", &val) == 1) { summ += val; } // Summ of all Numbers
 
-        for(int i = 0; i < 26; i++) // Loop : alphabet size
+        for(int i = 0; i < 26; i++)                        // Loop : alphabet size
         {
           if(converted - (i+65) == 0 || converted - (i + 97) == 0) 
-          // If current char ascii val. - (i+65) ascii val. = 0 | i = pos of ascii letter 
+          /*
+           Find letter in ASCII Table val. | i = pos of ASCII char.
+           e.g i = 0 = 65 = A, | i = 0 = 97 = a | same for 'a' = 97
+           Subtract converted char val. from i e.g char = 'A' -> 65 - (i=0 + 65) = 0
+           Save to pos[i] in array so we know i=0=A/a is on first pos in array e.g B/b is on pos[2] in array.
+          */ 
           {
-              letters[i] += 1; // Add 1 to i pos.
+              letters[i] += 1;                            // Add 1 to i pos.
           }
         }
       }      
   }
+
   // END ***************************************************************** 
 
   //START ****************************************************************
+
     printf("\n~ Saving results in [ output.txt ]\n");
     
-    f = fopen("output.txt", "w"); //save in output file
+    f = fopen("output.txt", "w");                        //change mode to write
 
+    // ERROR handling
     if (f == NULL){ printf("\nERROR 405 : [Unable to create output File]\n"); return; }
+   
     else
     {
-      // Print every letter with count
-      for (int i = 0; i < 26; i++)
+      for (int i = 0; i < 26; i++)                      // Print every letter with count
       {
         fprintf(f, "%lc: [ %d ]\n", (char)i+97, letters[i]); 
       }
       
       fprintf(f, "Summe aller Zahlen ist [ %d ]", summ);
 
-      fclose(f); // Close Filestream
+      fclose(f);                                        // Close Filestream
       printf("\nDone!\n");
     }
+    
     //END ****************************************************************
 }
 
